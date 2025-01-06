ba40998379edbcc5b272be144bcb0b07217305a6
jakob <noreply@email.com>
2025-01-06 18:50:34 +0100

    androidenv

--- flakes/flutter/flake.nix
+++ flakes/flutter/flake.nix
@@ -17,29 +17,40 @@
           allowUnfree = true;
         };
       };
-      buildToolsVersion = "33.0.2";
+      buildToolsVersion = "34.0.0";
       androidComposition = pkgs.androidenv.composeAndroidPackages {
         buildToolsVersions = [buildToolsVersion];
-        platformVersions = ["33"];
+        platformVersions = ["33" "34" "35"];
         abiVersions = ["arm64-v8a"];
       };
       androidSdk = androidComposition.androidsdk;
     in {
-      devShell = with pkgs;
-        mkShell rec {
+      devShells = with pkgs; {
+        default = mkShell {
           packages = [
             fish
           ];
           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          PATH = "${gst_all_1.gstreamer.dev}/bin";
+          GST_PLUGIN_SYSTEM_PATH = "${gst_all_1.gst-plugins-base}/lib/gstreamer-1.0/:${gst_all_1.gst-plugins-good}/lib/gstreamer-1.0/";
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/${buildToolsVersion}/aapt2";
           buildInputs = [
             flutter
             androidSdk
-            jdk11
+            jdk17
             pkg-config
             gtk3
+
+            glibc
+
+            gst_all_1.gstreamer.dev
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
           ];
 
           shellHook = "fish";
         };
+
+      };
     });
 }
