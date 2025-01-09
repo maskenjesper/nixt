782562bc6bfa439dc85fba81dd1bd5691c6153c3
jakob <noreply@email.com>
2025-01-09 23:54:50 +0100

    elixir flake

---
+++ flakes/elixir/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Elixir";
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs-unstable,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs-unstable {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in {
+      devShells = with pkgs; {
+        default = mkShell {
+          buildInputs = [
+            elixir
+          ];
+        };
+      };
+    });
+}
