512333e0ac5906107a35acb1383af1c507a47f71
jakob <noreply@email.com>
2025-01-30 23:45:09 +0100

    simple

--- flakes/phoenix/flake.nix
+++ flakes/phoenix/flake.nix
@@ -1,61 +1,29 @@
 {
-  description = "A flake for building development environment of Phoenix project.";
-
+  description = "Elixir";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = {
     self,
-    nixpkgs,
+    nixpkgs-unstable,
     flake-utils,
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        with pkgs; {
-          devShells.default = mkShell {
-            buildInputs =
-              [
-                erlangR25
-                beam.packages.erlangR25.elixir_1_14
-                nodejs-16_x
-              ]
-              ++ lib.optionals stdenv.isLinux [
-                # For ExUnit Notifier on Linux.
-                libnotify
-
-                # For file_system on Linux.
-                inotify-tools
-              ]
-              ++ lib.optionals stdenv.isDarwin [
-                # For ExUnit Notifier on macOS.
-                terminal-notifier
-
-                # For file_system on macOS.
-                darwin.apple_sdk.frameworks.CoreFoundation
-                darwin.apple_sdk.frameworks.CoreServices
-              ];
-
-            shellHook = ''
-              # allows mix to work on the local directory
-              mkdir -p .nix-mix
-              mkdir -p .nix-hex
-              export MIX_HOME=$PWD/.nix-mix
-              export HEX_HOME=$PWD/.nix-hex
-              export ERL_LIBS=$HEX_HOME/lib/erlang/lib
-
-              # concats PATH
-              export PATH=$MIX_HOME/bin:$PATH
-              export PATH=$MIX_HOME/escripts:$PATH
-              export PATH=$HEX_HOME/bin:$PATH
-
-              # enables history for IEx
-              export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.erlang-history\"'"
-            '';
-          };
-        }
-    );
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs-unstable {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in {
+      devShells = with pkgs; {
+        default = mkShell {
+          buildInputs = [
+            elixir
+            sqlite
+          ];
+        };
+      };
+    });
 }
